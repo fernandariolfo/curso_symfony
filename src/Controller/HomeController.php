3d9ae325fd11ceb4eef36140f360a0b8f938e3a3
Riolfo, María Fernanda <mfernandariolfo@gmail.com>
2021-01-31 01:25:42 -0300

    entrega 3

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,19 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Category;
+use App\Entity\Product;
+
 
 class HomeController extends AbstractController
 {
+
     /**
      * @Route("/home", name="home")
-     */
+     */ 
+
     public function index(): Response
+    {
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'Index entregas',
+
+        ]);
+        
+    }
+
+    /**
+     * @Route("/home/entrega_dos", name="home/entrega_dos")
+     */ 
+
+    public function entrega_dos(): Response
     {
 
         $variable_a_evaluar_numerica = 10;
         $valor_numerico = 20;
 
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/entrega_dos.html.twig', [
             'controller_name' => 'Entrega 2 - Ejercicio 3',
             'variable_a_evaluar_numerica' => $variable_a_evaluar_numerica,
             'valor_numerico' => $valor_numerico
@@ -49,6 +68,29 @@ class HomeController extends AbstractController
         ]);
 
     }
+
+    /**
+     * @Route("/home/entrega_tres", name="home/entrega_tres")
+     */ 
+
+    public function entrega_tres(): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $productos = $em->getRepository(Product::class)->findAll();
+        $categorias = $em->getRepository(Category::class)->findAll();
+        $productos_con_categoria_c = $em->getRepository(Product::class)->findByCategoria(3);
+
+        return $this->render('home/entrega_tres.html.twig', [
+            'controller_name' => 'Entrega 3 - Ejercicio 5',
+            'productos' => $productos,
+            'categorias' => $categorias,
+            'productos_con_categoria_c' => $productos_con_categoria_c,
+
+        ]);
+        
+    }
+
 
 }
 
---
+++ src/Entity/Product.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product", indexes={@ORM\Index(name="fk", columns={"categoria"})})
+ * @ORM\Entity
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=50, nullable=false)
+     */
+    private $nombre;
+
+    /**
+     * @var \Category
+     *
+     * @ORM\ManyToOne(targetEntity="Category")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="categoria", referencedColumnName="id")
+     * })
+     */
+    private $categoria;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getCategoria(): ?Category
+    {
+        return $this->categoria;
+    }
+
+    public function setCategoria(?Category $categoria): self
+    {
+        $this->categoria = $categoria;
+
+        return $this;
+    }
+
+
+}
